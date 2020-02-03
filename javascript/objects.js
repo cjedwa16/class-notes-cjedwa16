f35b1374fa9626cb7f49a99843a63b21fe049ba3
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-03 13:15:38 -0500

    JavaScript Arrays

--- javascript/objects.js
+++ javascript/objects.js
@@ -21,5 +21,25 @@ console.log(circle.center.x);
 
 // Undefined subproperties
 console.log(circle.color);  // this gives an undefined instead of console.error
-circle.color = 'blue'
+circle.color = 'blue';
 console.log(circle);
+
+// Array
+const languages = ['HTML', 'CSS', 'JS'];
+console.log(languages);
+
+// Accessing length and elements
+for (let i = 0; i< languages.length; i++)  {
+  console.log(languages[i]);
+}
+
+// Changing elements
+languages[2] = 'Javascript';
+
+// Adding elements
+languages.push('MongoDB');
+
+// Iterating over elements
+for (const lang of languages)  {
+  console.log(lang);
+}
