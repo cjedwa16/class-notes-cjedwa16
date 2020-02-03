0d10de46a6f6cd4d5e18bad04951f7d4e3820bab
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-03 13:07:17 -0500

    JavaScript object literals

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,5 +1,25 @@
 // Objects and array syntax
 
 // Object literal
-const point = {x: 1, y: 2}
-console.log(point)
+const point = {x: 1, y: 2};
+console.log(point);
+
+// Accessing properties
+console.log(point.x);
+
+// Changing properties
+point.x = 3;
+
+// Nested object literal
+const circle = {
+  center: point,
+  radius: 4
+};
+
+// Accessing subproperties
+console.log(circle.center.x);
+
+// Undefined subproperties
+console.log(circle.color);  // this gives an undefined instead of console.error
+circle.color = 'blue'
+console.log(circle);
