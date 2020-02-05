b7b2d80c685ddb019d36ffb61bdfdf65fe52c339
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-05 14:20:54 -0500

    filter method attempt

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -14,5 +14,5 @@ const filter1 = function(array, filterer)  {
 }
 
 const numbers = [1, 2, 3, 4, 5];
-odds = numbers.filter1(numbers, x => x%2 > 0);
+const odds = filter1(numbers, x => x%2 > 0);
 console.log(odds);
