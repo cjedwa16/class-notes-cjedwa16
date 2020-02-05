fb1f8b1518072632ab1567fda6671a5107080e2b
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-05 13:59:28 -0500

    JavaScript file input

--- javascript/input.js
+++ javascript/input.js
@@ -1,7 +1,7 @@
 // Syntax for console and file input
 
 const readline = require('readline')  // import statement
-
+const fs = require('fs');
 // Console configuration
 const user = readline.createInterface({
   input: process.stdin,
@@ -10,8 +10,19 @@ const user = readline.createInterface({
 
 // Console input
 user.question('Enter a filename: ', function(filename)  {
-  console.log(filename);
-});
 
-// This part isn't after the console input
-console.log('here');
+  // File configuration
+  const file = readline.createInterface({
+    input: fs.createReadStream(filename)
+  });
+
+  // Asynchronous line-by-line input
+  file.on('line', function(line){
+    console.log(line);
+  });
+
+  // End the program when the file closes
+  file.on('close', function()  {
+    process.exit(0);
+  });
+});
