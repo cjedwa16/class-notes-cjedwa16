195df1e078ae1663f5b2221f22289cd98b637e4c
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-05 13:46:23 -0500

    JavaScript console input

--- javascript/input.js
+++ javascript/input.js
@@ -3,7 +3,7 @@
 const readline = require('readline')  // import statement
 
 // Console configuration
-const user = readine.createInterface({
+const user = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
