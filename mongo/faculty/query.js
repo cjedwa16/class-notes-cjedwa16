51fcc1f7729edb3cc8a0c73ff753012755d586dd
Cameron Edwards <cjedwa16@stlawu.edu>
2020-02-12 13:23:44 -0500

    Typical MongoDB queries

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -6,10 +6,36 @@ const Professor = require('./schema');
 
 connect(); // To the database
 
-// What documents are in the collection?
+/*// What documents are in the collection?
 const query = Professor.find();
 query.exec(function(error, professors)  {
   if (error) console.error(error.stack);
   console.log(professors);
 
+});*/
+
+const queries = [
+
+// What are names in alphabetical order?
+Professor.find().sort('name'),
+
+// Who started most recently? // sort started in reverse order (most recent)
+Professor.find().sort('-started').limit(1),
+
+// Who started in 2003?
+Professor.find().where('started').equals(2003),
+
+// Who teaches 362?
+Professor.find().where('courses').in(362),
+
+// What are all the ranks?
+Professor.distinct('rank')
+];
+
+// Professor.find().sort('name')
+queries[0].exec(function(error, professors)  {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name)
+  console.log('Names in order: ', names);
 });
