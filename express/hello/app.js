7b9bc5141d22470509a605d1e9778397d4507c49
Cameron Edwards <cjedwa16@stlawu.edu>
2020-03-04 13:35:25 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,10 +4,35 @@ const express = require('express');
 // Create the server
 const app = express();
 
+
+// cs-linuxlab-40.stlawu.edu:3000/
 // Respond to one request
 app.get('/', function(request, response)  {
   response.send('Hello Cam');
 });
+
+// cs-linuxlab-40.stlawu.edu:3000/foo
+app.get('/foo', function(request, response)  {
+  response.send('Hello foo');
+});
+
+// cs-linuxlab-40.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response)  {
+  response.send('Hello bar');
+});
+
+// cs-linuxlab-40.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response)  {
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-40.stlawu.edu:3000/zap?x=&y=
+app.get('/zap', function(request, response)  {
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
+});
+
+
+
 
 // Start the server
 app.listen(3000);
