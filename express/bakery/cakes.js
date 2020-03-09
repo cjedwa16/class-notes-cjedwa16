8b1e8d3c5b16c905cf38eefdd6002a0daf09505b
Cameron Edwards <cjedwa16@stlawu.edu>
2020-03-09 13:52:58 -0400

    fix

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -2,39 +2,36 @@
 const express = require('express');
 
 // Create the router
-const router = express.Router()
+const router = express.Router();
 
 // Pretend database collection
 const cakes = [
-  {id: 'vanilla', flavor:'Vanilla'},
-  {id: 'ch', flavor:'Chocolate'}
+  {id: 'vanilla', flavor: 'Vanilla'},
+  {id: 'chocolate', flavor: 'Chocolate'}
 ];
 
-// cs-linuxlab-40.stlawu.edu:3000/cakes/
-router.get('/', function(request, response)  {
+// cs-linuxlab-##.stlawu.edu:3000/cakes/
+router.get('/', function(request, response) {
   response.send(`
     <h1>Cakes</h1>
     <ul>
       <li><a href="/cakes/vanilla">Vanilla</a></li>
       <li><a href="/cakes/chocolate">Chocolate</a></li>
     </ul>
-    `);
+  `);
 });
 
-// cs-linuxlab-40.stlawu.edu:3000/cakes/id
-router.get('/:id', function(request, response, next)  {
+// cs-linuxlab-##.stlawu.edu:3000/cakes/id
+router.get('/:id', function(request, response, next) {
 
   // Pretend database lookup
   const cake = cakes.find(cake => cake.id === request.params.id);
 
-  if (!cake)  {
-    next();  // pass on this request (404 error in app.js)
-  } else  {
+  if (!cake) {
+    next(); // Pass on this request
+  } else {
     response.render('cakes/detail', {cake: cake});
   }
-
-
-
 });
 
 module.exports = router;
