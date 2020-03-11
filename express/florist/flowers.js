003256f8cae47962d60765d08f4068cfd977ba20
Cameron Edwards <cjedwa16@stlawu.edu>
2020-03-11 13:49:29 -0400

    ajax

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -10,32 +10,55 @@ let flowers = [
   {id: 'rose', color: 'red', season: 'summer'}
 ];
 
-
 // GET /flowers
-router.get('/', function(request, response)  {
+router.get('/', function(request, response) {
   response.send(flowers);
-})
+});
 
 // GET /flowers/id
-router.get('/:id', function(request, response, next)  {
+router.get('/:id', function(request, response, next) {
   const flower = flowers.find(f => f.id === request.params.id);
-  if (!flower)  {
-    next();  // leads to 404
-  } else  {
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
     response.send(flower);
   }
 });
 
 // POST /flowers
-router.post('/', function(request, response)  {
+router.post('/', function(request, response) {
   const flower = request.body;
-  if (flowers.find(f => f.id == flower.id))  {
-    response.status(400).end(); // dont recreate something that already exists
+  if (!flower.id) {
+    response.status(400).send('Missing ID');
+  } else if (flowers.find(f => f.id === flower.id)) {
+    response.status(400).send('Duplicate ID');
+  } else {
+    flowers.push(flower);
+    response.status(201).send(flowers);
   }
-  flowers.push(flower);
-  response.status(201).end(flowers);
 });
 
 // DELETE /flowers/id
+router.delete('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
+    flowers = flowers.filter(f => f.id !== flower.id);
+    response.status(200).send(flowers);
+  }
+});
 
 // PUT /flowers/id
+router.put('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
+    if (request.body.color) flower.color = request.body.color;
+    if (request.body.season) flower.season = request.body.season;
+    response.status(200).send(flowers);
+  }
+});
+
+module.exports = router;
